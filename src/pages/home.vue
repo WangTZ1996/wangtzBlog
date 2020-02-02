a9e38ad8abcffdbb28e10c51d33e164b311cd1ee
WangTZ1996 <2249796929@qqcom>
2020-02-02 21:22:36 +0800

    初始化首页

---
+++ src/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="world"></div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style scoped>
+#world {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: linear-gradient(#e4e0ba, #f7d9aa);
+  /*cursor: url('C:/Users/Administrator/Desktop/wtzblog2.0/asset/images/point.png'),auto;*/
+}
+</style>
